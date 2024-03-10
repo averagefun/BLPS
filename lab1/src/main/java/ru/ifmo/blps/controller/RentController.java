131a64465f43b87b4aeff1a71a30df3ace2a235c
poma12390 <kochnevr21@gmail.com>
2024-03-10 03:00:29 +0300

    Edit model

--- lab1/src/main/java/ru/ifmo/blps/controller/RentController.java
+++ lab1/src/main/java/ru/ifmo/blps/controller/RentController.java
@@ -1,13 +1,12 @@
 package ru.ifmo.blps.controller;
 
+import org.openapitools.model.ListingRequest;
 import org.openapitools.model.ListingsResponse;
-import org.openapitools.model.RentListingRequest;
 import org.openapitools.model.RentListingResponse;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
@@ -23,7 +22,7 @@ public class RentController {
     }
 
     @PostMapping("/rent/listings")
-    public ResponseEntity<RentListingResponse> createRentListing(@RequestBody RentListingRequest request) {
+    public ResponseEntity<RentListingResponse> createRentListing(@RequestBody ListingRequest request) {
         // Здесь должна быть логика создания объявления на аренду
         return ResponseEntity.ok(new RentListingResponse());
     }
--- lab1/src/main/java/ru/ifmo/blps/controller/SalesController.java
+++ lab1/src/main/java/ru/ifmo/blps/controller/SalesController.java
@@ -1,15 +1,13 @@
 package ru.ifmo.blps.controller;
 
-import lombok.extern.log4j.Log4j;
 import lombok.extern.slf4j.Slf4j;
+import org.openapitools.model.ListingRequest;
 import org.openapitools.model.ListingsResponse;
-import org.openapitools.model.SaleListingRequest;
 import org.openapitools.model.SaleListingResponse;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
@@ -19,15 +17,16 @@ import java.util.List;
 @RestController
 public class SalesController {
 
-    @PostMapping("/sale/listings")
-    public ResponseEntity<SaleListingResponse> createSaleListing(@RequestBody SaleListingRequest request) {
-        // Здесь должна быть логика создания объявления на продажу
-        return ResponseEntity.ok(new SaleListingResponse());
-    }
     @GetMapping("/sale/listings")
     public ResponseEntity<List<ListingsResponse>> getSaleListings() {
         // Здесь должна быть логика получения списка объявлений на продажу
         log.info("HIII");
         return ResponseEntity.ok(new ArrayList<>());
+    }
+
+    @PostMapping("/sale/listings")
+    public ResponseEntity<SaleListingResponse> createSaleListing(@RequestBody ListingRequest request) {
+        // Здесь должна быть логика создания объявления на продажу
+        return ResponseEntity.ok(new SaleListingResponse());
     }
 }
