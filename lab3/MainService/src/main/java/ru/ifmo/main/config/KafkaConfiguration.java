e3db62b3e71b05cb6f27bf09484f0dba4266b8ae
averagefun <andrew.valid@yandex.ru>
2024-05-07 23:12:52 +0300

    MainService: kafka config & dependencies

---
+++ lab3/MainService/src/main/java/ru/ifmo/main/config/KafkaConfiguration.java
@@ -0,0 +1,35 @@
+package ru.ifmo.main.config;
+
+import java.util.Map;
+
+import lombok.RequiredArgsConstructor;
+import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.core.ProducerFactory;
+
+@Configuration
+@RequiredArgsConstructor
+public class KafkaConfiguration {
+    private final KafkaProperties kafkaProperties;
+
+    @Bean
+    public ProducerFactory<String, String> producerFactory() {
+        Map<String, Object> properties = kafkaProperties.buildProducerProperties(null);
+        // todo: bootstrap.servers
+        return new DefaultKafkaProducerFactory<>(properties);
+    }
+
+    @Bean
+    public KafkaTemplate<String, String> kafkaTemplate() {
+        return new KafkaTemplate<>(producerFactory());
+    }
+
+    @Bean
+    public NewTopic resumeCheckTopic() {
+        return new NewTopic("user-check", 1, (short) 1);
+    }
+}
